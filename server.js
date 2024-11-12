f0be09d231fb0e29da6eef5a16c733d6a4f08c87
User Name <jsalcedo19@gmail.com>
2024-11-12 11:28:50 -0500

    exercise 2

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-import express from 'express'
+import express, { request } from 'express'
 const app = express()
 //exercise 1:
 app.get('/greetings/:name', (req,res) => {
 res.send(`<h1>Greetings${req.params.name}.<h1>`)
 })
 
-
-
+//exercise 2:
+app.get('/roll/:number', (req,res) => {
+    let number = Number(req.params.number);
+    res.send(`<h1> You Roll! ${Math.ceil(Math.random()*number)} on a d${number}.</h1>`)
+})
 
 
 
